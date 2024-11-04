33858a85145616f0f338a8ab67fb69ab4760d228
Meli <amelie.bodiou@supdevinci-edu.fr>
2024-11-04 14:02:09 +0100

    maj

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -3,18 +3,7 @@ import Navbar from '../components/Navbar';
 import MenuList from '../components/MenuList';
 import api from '../services/api';
 import '../styles/HomePage.css';
-import axios from 'axios';
-import { API_URL } from '../config';
 
-export const fetchFoodTrucks = async () => {
-  try {
-    const response = await axios.get(`${API_URL}/food_trucks`);
-    return response.data;
-  } catch (error) {
-    console.error("Erreur lors de la récupération des food trucks :", error);
-    throw error;
-  }
-};
 const HomePage = () => {
   const [items, setItems] = useState([]);
 
