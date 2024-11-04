f65cb11d88b73cf20483baf7615da001c99da480
JohanLhg <johan.lhg@outlook.fr>
2024-11-04 16:49:13 +0100

    feat(auth): form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AuthPage from './pages/AuthPage';
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
 import HomePage from './pages/HomePage';
 import ResaPage from './pages/ResaPage';
 
@@ -8,7 +9,8 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<AuthPage />} />
+        <Route path="/" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/resa" element={<ResaPage />} />
       </Routes>
